001ff428691c8a09374f07f1d536d2c7b8a0f835
Rishabh Raghwendra <rishabhraghwendra2002@gmail.com>
2022-08-29 12:11:14 +0530

    removed comments from contract

--- contracts/contracts/MyFlashLoan.sol
+++ contracts/contracts/MyFlashLoan.sol
@@ -9,13 +9,6 @@ import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contract
 import {SafeMath} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/SafeMath.sol";
 import "hardhat/console.sol";
 
-// interface IFaucet {
-//     function mint(
-//         address _token,
-//         uint256 _amount
-//     ) external;
-// }
-
 library StringHelper {
     function concat(bytes memory a, bytes memory b)
         internal
@@ -46,14 +39,6 @@ abstract contract FlashLoanSimpleReceiverBase is IFlashLoanSimpleReceiver {
     IPoolAddressesProvider public immutable override ADDRESSES_PROVIDER;
     IPool public immutable override POOL;
 
-    //   IFaucet public immutable FAUCET;
-
-    //   constructor(IPoolAddressesProvider provider, IFaucet faucet) {
-    //     ADDRESSES_PROVIDER = provider;
-    //     POOL = IPool(provider.getPool());
-    //     FAUCET = faucet;
-    //   }
-
     constructor(IPoolAddressesProvider provider) {
         ADDRESSES_PROVIDER = provider;
         POOL = IPool(provider.getPool());
@@ -77,13 +62,6 @@ contract MySimpleFlashLoanV3 is FlashLoanSimpleReceiverBase {
     address payable public owner;
     uint public gasPrice;
     uint public value;
-    string val;
-
-    // constructor(IPoolAddressesProvider _addressProvider)
-    //     FlashLoanSimpleReceiverBase(_addressProvider)
-    // {
-    //     owner = payable(msg.sender);
-    // }
 
     constructor(IPoolAddressesProvider _addressProvider)
         FlashLoanSimpleReceiverBase(_addressProvider)
@@ -140,8 +118,8 @@ contract MySimpleFlashLoanV3 is FlashLoanSimpleReceiverBase {
         require(success, string(bytes('SWAP CALL FAILED: ').concat(bytes(data.getRevertMsg()))));
         console.log("swaped");
         // Refund any unspent protocol fees to the sender.
-        // msg.sender.transfer(address(this).balance);
-        // // Use our current buyToken balance to determine how much we've bought.
+        payable(msg.sender).transfer(address(this).balance);
+        // Use our current buyToken balance to determine how much we've bought.
         // boughtAmount = buyToken.balanceOf(address(this)) - boughtAmount;
         // emit BoughtTokens(sellToken, buyToken, boughtAmount);
     }
@@ -164,48 +142,12 @@ contract MySimpleFlashLoanV3 is FlashLoanSimpleReceiverBase {
         console.log("sell token amount: ",sellToken.balanceOf(address(this)));
         fillQuote();
 
-        // At the end of your logic above, this contract owes
-        // the flashloaned amounts + premiums.
-        // Therefore ensure your contract has enough to repay asset: 0xA2025B15a1757311bfD68cb14eaeFCc237AF5b43
-        // assetprovider: 0xc4dCB5126a3AfEd129BC3668Ea19285A9f56D15D
-        // faucet: 0x1ca525Cd5Cb77DB5Fa9cBbA02A0824e283469DBe
-        // these amounts.
-        // Approve the LendingPool contract allowance to *pull* the owed amount
+        // after swap payback the loan
         uint amountOwed = amount.add(premium);
-        // FAUCET.mint(asset,1000000000000);
         IERC20(asset).approve(address(POOL), amountOwed);
         // widthdrawAmount();
         return true;
     }
-
-    // function setter(
-    //     address asset,
-    //     uint256 amount,
-    //     IERC20 _sellToken,
-    //     IERC20 _buyToken,
-    //     address allowanceTarget,
-    //     address payable _to,
-    //     uint _gasPrice,
-    //     uint _value
-    // ) public payable {
-    //     address receiverAddress = address(this);
-
-    //     bytes memory params = "";
-    //     uint16 referralCode = 0;
-
-    //     spender = allowanceTarget;
-    //     swapTarget = _to;
-    //     gasPrice = _gasPrice;
-    //     value = _value;
-    //     console.log("calling pool: ");
-    //     POOL.flashLoanSimple(
-    //         receiverAddress,
-    //         asset,
-    //         amount,
-    //         params,
-    //         referralCode
-    //     );
-    // }
 
     function executeFlashLoan(
         address asset,
@@ -235,9 +177,6 @@ contract MySimpleFlashLoanV3 is FlashLoanSimpleReceiverBase {
         gasPrice = _gasPrice;
         value = _value;
         value = 123456789;
-        val = "123456789";
-        console.log("sellToken: ", msg.value);
-        // require(address(0)!=spender,"No address provided to spender");
 
         POOL.flashLoanSimple(
             receiverAddress,
